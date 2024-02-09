2166b7c1c537fd3e7900363931519fb338071580
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-02-09 23:07:54 +0300

    Navbar

--- react-game/src/components/Navbar.js
+++ react-game/src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Navbar = ({score}) => {
+import logo from '../logo.svg';
+
+const Navbar = ({score, nextShape}) => {
     return ( <nav>
-        
+        <div>
+            <img src={logo} alt='Tetris Logo' />
+        </div>
+        <div>
+            {nextShape}
+        </div>
+        <div>
+            <span>Score</span> <span className='score-counter'>{score}</span>
+        </div>
     </nav> );
 }
  
