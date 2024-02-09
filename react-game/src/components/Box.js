332019024762f5704ebc3f5dd3608aa1e0eab5e7
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-02-09 23:07:39 +0300

    Box element

--- react-game/src/components/Box.js
+++ react-game/src/components/Box.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const Box = ({color}) => {
+const Box = ({color, gap, x, y}) => {
     return ( 
-        <rect >
-            
-        </rect>
+        <rect height={gap} width={gap} x={x} y={y} fill={color} />
      );
 }
  
