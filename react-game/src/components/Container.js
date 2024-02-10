dd232970f474c2c462921426cd4060ef62cc939c
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-02-10 23:32:28 +0300

    container

--- react-game/src/components/Container.js
+++ react-game/src/components/Container.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Background from './Background';
-import ShapeL from './shapes/ShapeL';
 
 const Container = ({rows, columns, gap}) => {
     return ( <div className='svg-container'>
         <svg viewBox={`0 0 ${columns * gap} ${rows * gap}`}>
         <Background rows={rows} columns={columns} gap={gap}/>
 
-        <ShapeL row={rows/2} column={0} gap={gap}  />
     </svg>
     </div> );
 }
